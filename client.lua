49817cedb1359d89fff0bc41297b1d40450fba46
Tyler Abel <lcpdpolice4939@gmail.com>
2025-11-12 23:13:48 -0500

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,71 +1,10 @@
 local insideApartment = false
 local currentApt = nil
+local exitPos = vector3(266.0, -1007.0, -101.0) -- fallback; will update when entering
+local entrancePos = nil
 
-CreateThread(function()
-    while true do
-        local sleep = 1000
-        local ped = PlayerPedId()
-        local pos = GetEntityCoords(ped)
-
-        for id, apt in pairs(Config.Apartments) do
-            local dist = #(pos - apt.entrance)
-            if dist < 2.0 then
-                sleep = 0
-                DrawText3D(apt.entrance.x, apt.entrance.y, apt.entrance.z, "[E] Enter | [G] Buy (" .. apt.price .. ")")
-                if IsControlJustPressed(0, 38) then -- E
-                    TriggerServerEvent("apartment:enter", id)
-                elseif IsControlJustPressed(0, 47) then -- G
-                    TriggerServerEvent("apartment:buy", id)
-                end
-            end
-        end
-        Wait(sleep)
-    end
-end)
-
-RegisterNetEvent("apartment:entered", function(interiorCoords, heading)
-    local ped = PlayerPedId()
-    SetEntityCoords(ped, interiorCoords)
-    SetEntityHeading(ped, heading)
-    insideApartment = true
-end)
-
-RegisterNetEvent("apartment:exited", function()
-    insideApartment = false
-    -- optionally teleport to entrance
-    -- handled on server if needed
-end)
-
--- handle invite
-RegisterNetEvent("apartment:invited", function(ownerId, apartmentId, bucket)
-    local apt = Config.Apartments[apartmentId]
-    TriggerEvent("chat:addMessage", {
-        args = {"Apartment", "Player " .. ownerId .. " invited you to their apartment! Press [E] to accept."}
-    })
-
-    CreateThread(function()
-        local start = GetGameTimer()
-        while GetGameTimer() - start < 10000 do -- 10 seconds
-            if IsControlJustPressed(0, 38) then
-                TriggerServerEvent("apartment:acceptInvite", ownerId, apartmentId, bucket)
-                return
-            end
-            Wait(0)
-        end
-    end)
-end)
-
--- Exit key inside apartment
-CreateThread(function()
-    while true do
-        Wait(0)
-        if insideApartment and IsControlJustPressed(0, 38) then
-            TriggerServerEvent("apartment:exit")
-        end
-    end
-end)
-
-function DrawText3D(x, y, z, text)
+-- Draw Text Helper
+local function DrawText3D(x, y, z, text)
     SetDrawOrigin(x, y, z, 0)
     SetTextScale(0.35, 0.35)
     SetTextFont(4)
@@ -77,3 +16,77 @@ function DrawText3D(x, y, z, text)
     DrawText(0.0, 0.0)
     ClearDrawOrigin()
 end
+
+-- Main loop for apartment entrances
+CreateThread(function()
+    while true do
+        local sleep = 1000
+        local ped = PlayerPedId()
+        local pos = GetEntityCoords(ped)
+
+        if not insideApartment then
+            for id, apt in pairs(Config.Apartments) do
+                local dist = #(pos - apt.entrance)
+                if dist < 2.0 then
+                    sleep = 0
+                    DrawText3D(apt.entrance.x, apt.entrance.y, apt.entrance.z, "[E] Enter | [G] Buy ($" .. apt.price .. ")")
+                    if IsControlJustPressed(0, 38) then -- E
+                        entrancePos = apt.entrance
+                        TriggerServerEvent("apartment:enter", id)
+                    elseif IsControlJustPressed(0, 47) then -- G
+                        TriggerServerEvent("apartment:buy", id)
+                    end
+                end
+            end
+        else
+            -- Inside apartment: show exit marker
+            local ped = PlayerPedId()
+            local pos = GetEntityCoords(ped)
+            local dist = #(pos - exitPos)
+
+            if dist < 2.0 then
+                sleep = 0
+                DrawText3D(exitPos.x, exitPos.y, exitPos.z, "[E] Exit Apartment")
+                if IsControlJustPressed(0, 38) then
+                    TriggerServerEvent("apartment:exit")
+                end
+            end
+        end
+
+        Wait(sleep)
+    end
+end)
+
+-- When server tells player they entered apartment
+RegisterNetEvent("apartment:entered", function(interiorCoords, heading)
+    SetEntityCoords(PlayerPedId(), interiorCoords)
+    SetEntityHeading(PlayerPedId(), heading)
+    exitPos = interiorCoords
+    insideApartment = true
+end)
+
+-- When server confirms exit
+RegisterNetEvent("apartment:exited", function()
+    insideApartment = false
+    if entrancePos then
+        SetEntityCoords(PlayerPedId(), entrancePos)
+    end
+    TriggerEvent("QBX:Notify", "You have exited your apartment.", "inform")
+end)
+
+-- Handle invites (unchanged)
+RegisterNetEvent("apartment:invited", function(ownerId, apartmentId, bucket)
+    local apt = Config.Apartments[apartmentId]
+    TriggerEvent("QBX:Notify", "Player " .. ownerId .. " invited you to their apartment. Press [E] to accept.", "inform")
+
+    CreateThread(function()
+        local start = GetGameTimer()
+        while GetGameTimer() - start < 10000 do
+            if IsControlJustPressed(0, 38) then
+                TriggerServerEvent("apartment:acceptInvite", ownerId, apartmentId, bucket)
+                return
+            end
+            Wait(0)
+        end
+    end)
+end)
